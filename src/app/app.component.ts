cb7880d1a827102017636c27a1256578ce0122a4
Kirandinesh <kirandinesh646@gmail.com>
2025-04-16 22:00:39 +0530

    new

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,9 +3,15 @@ import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './components/header/header.component';
 
 import { FooterComponent } from './components/footer/footer.component';
+import { HomeComponent } from "./components/home/home.component";
+import { AboutComponent } from "./components/about/about.component";
+import { SkillsComponent } from "./components/skills/skills.component";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { CareerComponent } from "./components/career/career.component";
+import { ContactComponent } from "./components/contact/contact.component";
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, FooterComponent],
+  imports: [RouterOutlet, HeaderComponent, FooterComponent, HomeComponent, AboutComponent, SkillsComponent, ProjectsComponent, CareerComponent, ContactComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
